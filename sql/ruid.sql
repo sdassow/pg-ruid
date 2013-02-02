532488b39f0f8a65decd2ce6c93c0934988df84a
Simon Bertrang <janus@errornet.de>
2013-02-02 14:53:05 +0100

    adjust copyright/license

--- sql/ruid.sql
+++ sql/ruid.sql
@@ -1,27 +1,18 @@
---
---  ruid - Readable Unique Identifier
---
---  Copyright (c) 2004-2007 Ralf S. Engelschall <rse@engelschall.com>
---  Copyright (c) 2004-2007 The OSSP Project <http://www.ossp.org/>
---
---  Permission to use, copy, modify, and distribute this software for
---  any purpose with or without fee is hereby granted, provided that
---  the above copyright notice and this permission notice appear in all
---  copies.
---
---  THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
---  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
---  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
---  IN NO EVENT SHALL THE AUTHORS AND COPYRIGHT HOLDERS AND THEIR
---  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
---  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
---  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
---  USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
---  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
---  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
---  OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
---  SUCH DAMAGE.
---
+/*
+ * Copyright (c) 2010-2013 Simon Bertrang <janus@errornet.de>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
 
 SET search_path TO public;
 SET client_min_messages TO warning;
